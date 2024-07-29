111ec4fb4557136e8d53dd4bd50f21a3b1c4487d
Arzoo Shamim <166494063+arzooshamim13@users.noreply.github.com>
2024-07-29 06:50:04 +0500

    class 1 to 15

---
+++ script.js
@@ -0,0 +1,193 @@
+console.log("HELLO ARZOO");
+ console.log('THIS IS 15 CHAPTERS PRECTICE') 
+
+// 1. Alerts
+// 2. Variables for Strings
+// 3. Variables for Numbers
+// 4. Variable Names Legal and Illegal
+// 5. Math Expressions: familiar operators
+// 6. Math Expressions: unfamiliar operators
+// 7. Math Expressions: eliminating ambiguity
+// 8. Concatenating text strings
+// 9. Prompts
+// 10. if statements
+// 11. Comparison operators
+// 12. if...else and else if statements
+// 13. Testing sets of conditions
+// 14. if statements nested
+// 15. Arrays
+
+
+
+
+
+//chapter 1 
+
+// alert("hello user");
+
+// var read = 'book';
+// var verb = ' is in the bed';
+// alert(read + verb);
+
+// chapter 2
+
+// var name = 'arzoo';
+// var father = 'shamim';
+// console.log(name + " " + father);
+
+
+//chapter 3
+
+// var yourDay = 2;
+// var yourMonth = 6; 
+// var yourYear = 2010;
+// console.log('My Date Of Birth Is'+ " " + yourDay + " - " +yourMonth +" - "+ yourYear )
+
+
+// chapter 4 legal and in legal  
+
+
+//chapter 5
+
+// plus
+// var num = 12;
+// var number = 13;
+// var outPut = num + number;
+// console.log(outPut);
+
+// minace
+// var min = 100;
+// var minace = 50;
+// var result = min - minace;
+// console.log(result);
+
+// multiply
+// var first = 10;
+// var multiply = 20;
+// var result = first * multiply;
+// console.log(result);
+
+// divied
+// var first = 20;
+// var sec = 10;
+// var result = first / sec;
+// console.log(result);
+
+// % is 0 all values out put is 0;
+
+
+// chapter 6
+
+// pre increment num++ , pre decrement num--
+// post increment ++num , post decrement --num
+
+// var num = 16;
+// num++;
+// console.log(num) // answer is 17
+
+
+// var num = 12;
+// num--;
+// console.log(num) // answer is 11
+
+
+// var num = 16;
+// ++num;
+// console.log(num) // answer is 17
+
+
+// var num = 12;
+// --num;
+// console.log(num) // answer is 11
+
+
+
+// chapter 7 
+// this is BODMAS rule
+
+// B = breckets
+// O = orders
+// D = division
+// M = multiplication
+// A = addition
+// S = subcription
+
+//  var number = 10 + 20 * 30 - 40; 
+//   console.log(number) answer is 570
+
+
+// chapter 8
+
+// alert('thank you for your input' + '! ')
+
+
+// chapter 9
+
+// var name = prompt("Enter your name");
+// // console.log(name);
+// document.write(name)
+
+
+
+// chapter 10 and chapter 12
+// var pop = prompt("enter Your name")
+// var name = "arzoo";
+
+// if(pop === "arzoo"){
+//     alert("MACTH USER")
+// }
+// else if(pop === "alishba"){
+//     alert('user is pending')
+// }
+// else{
+//     alert('NOT MATCH')
+// }
+
+
+// chapter 11
+
+// var num = 20;
+// var chota = 10;
+// if(num > chota ){
+//     console.log('bara hy')
+// }
+// var num = 30;
+// var chota = 40;
+// if(num < chota ){
+//     console.log('chota hy')
+// }
+// var num = 20;
+// var chota = 20;
+// if(num => chota ){
+//     console.log('baraber hy')
+// }
+// var num = 50;
+// var chota = 30;
+// if(num >= chota ){
+//     console.log('bara hy')
+// }
+
+
+// chapter 13
+
+// var name = "rehman";
+// var no = '';
+// var not = 10;
+
+// if (name => no || not > 1 ){
+// console.log('game out')
+// }
+
+//chapter 14 is nested if
+
+// chapter 15 
+
+// var arr = ['hello','world'];
+//  arr[2] = 'wide'; 
+// console.log(arr[2])
+
+
+
+
+
+
